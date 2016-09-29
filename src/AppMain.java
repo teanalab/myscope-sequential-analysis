879007862d834b808d2bbb55c21b07a14f46ca74
Niloy Hasan <Niloy Hasan>
2016-09-29 16:49:16 -0400

    Added an indication of finished execution.

--- src/AppMain.java
+++ src/AppMain.java
@@ -3,5 +3,6 @@ public class AppMain {
 	public static void main(String[] args) throws Exception {
 		Model model = new Model();
 		model.evaluateModelBy3Folds();
+		System.out.println("Finished execution.");
 	}
 }
