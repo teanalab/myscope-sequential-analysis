5eb8e6078ee580852a555ba9eb624d9acc1cfdab
Mehedi Hasan <mehedi@wayne.edu>
2017-02-26 19:58:06 -0500

    update introduction

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,14 +1,14 @@
 public class AppMain {
 	public static void main(String[] args) throws Exception {
-		Model model = new Model();
-		model.evaluateModelByKFolds(5, false, 2);
+		//Model model = new Model();
+		//model.evaluateModelByKFolds(5, false, 2);
 		//model.printTopKSuccessAndFailureSequence(20,false);
 		//model.provideFrequencyDistributionOfSequence(false);
-		System.out.println("\nFinished execution.");
+		//System.out.println("\nFinished execution.");
 		
-		//preprocess dmc files
-		//WordtoText.wordToText("c:/dmc/rawfiles", "c:/dmc/input");
-		//UtilityClass.preprocessMITranscripts("c:/dmc/input", "c:/dmc/output/");
+		// Preprocess dmc files
+		WordtoText.wordToText("c:/dmc/rawfiles", "c:/dmc/input");
+		UtilityClass.preprocessMITranscripts("c:/dmc/input", "c:/dmc/output/");
 		
 		//CRFClassifier crfModel = new CRFClassifier();
 		//crfModel.evaluateModel(5);
